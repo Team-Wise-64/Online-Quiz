b52d195c43bc3b2f28feee16fff6f44386a8ac9a
essa-essa <essa.essa@transferwise.com>
2023-06-11 23:01:33 +0100

    basic project structure

--- quiz-frontend/public/react/index.js
+++ quiz-frontend/public/react/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime'
 
 import {App} from './components/App';
 
