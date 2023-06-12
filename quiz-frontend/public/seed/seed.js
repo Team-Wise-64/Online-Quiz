f8d84710b2fb752d3c89fac2e1dd9145aa8833cc
essa-essa <essa.essa@transferwise.com>
2023-06-12 15:34:12 +0100

    database seed created

---
+++ quiz-frontend/public/seed/seed.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql2');
+const quizzes = require("./seedData.js")
+
+const connection = mysql.createConnection({
+  host: 'localhost', // Replace with your database host
+  port: 3306,
+  user: 'root', // Replace with your database username
+  password: 'password', // Replace with your database password
+  database: 'emp' // Replace with your database name
+});
+
+Object.keys(quizzes).forEach((quiz, index) => {
+  connection.query(`CREATE TABLE ${quiz} (question_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), a VARCHAR(255), b VARCHAR(255), c VARCHAR(255), d VARCHAR(255), answer VARCHAR(255));`)
+  
+  quizzes[quiz].forEach((item) => {
+    connection.query(`INSERT INTO ${quiz} SET ?`,item, (error, results) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Data inserted successfully');
+    }
+  });
+  })
+  
+});
+/* connection.query(`CREATE TABLE test (question_number INT NOT NULL AUTO_INCREMENT PRIMARY KEY, question VARCHAR(255), a VARCHAR(255), b VARCHAR(255), c VARCHAR(255), d VARCHAR(255), answer VARCHAR(255));`)
+// ww2.forEach((item) => {
+//     connection.query('INSERT INTO test SET ?', item, (error, results) => {
+//       if (error) {
+//         console.error(error);
+//       } else {
+//         console.log('Data inserted successfully');
+//       }
+//     });
+//   });
+*/
+
+  connection.end();
