3c3b13b49123e8604732b30822aadc17551a1a21
Paul Preda <paul.preda@transferwise.com>
2023-06-13 12:15:10 +0100

    Created the basic app template and a play button

--- quiz-frontend/public/react/components/App.js
+++ quiz-frontend/public/react/components/App.js
@@ -1,15 +1,15 @@
 import apiURL from '../api';
-import React, {useState} from "react"
-// import { Button } from '@mui/material';
+import React, { useState } from "react";
+import Play from "./allquiz";
+import '../../style.css';
+
 export const App = () => {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        Hello World
-        {/* <Button variant="contained"> Search for a quiz </Button>
-        <Button variant="contained"> Create a quiz </Button> */}
+        <h1>Welcome to the Mind Quest!</h1>
+        <Play />
       </header>
     </div>
   );
 }
-
