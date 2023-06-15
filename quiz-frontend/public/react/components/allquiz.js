64a6fa3be719128983baa716d3db1e540ad622bf
Salma Jama <salma.jama@transferwise.com>
2023-06-15 10:00:41 +0100

    Added new event that links playingquiz and allquiz page

--- quiz-frontend/public/react/components/allquiz.js
+++ quiz-frontend/public/react/components/allquiz.js
@@ -1,16 +1,22 @@
 import React from "react";
 
-export default function QuizList() {
+export default function QuizList({setState}) {
   const dummyQuizzes = [
     { quiz_number: 1, quiz_name: "ww2" },
     { quiz_number: 2, quiz_name: "cats" },
     { quiz_number: 3, quiz_name: "space" }
   ];
 
+  function quizClick(e){
+    e.preventDefault();
+    setState("playingQuiz")
+  }
+
   return (
     <div>
-    <p>Pick your quiz!</p>
-      {dummyQuizzes.map((quiz) => (<button key={quiz.quiz_number}>{quiz.quiz_name}</button>))}
+    <p>Pick your quz!</p>
+      {dummyQuizzes.map((quiz) => (<button key={quiz.quiz_number} onClick={quizClick}>{quiz.quiz_name}</button>))}
+  
     </div>
   )
 }
