bf5ef6e7d9a13c7feed602153de936033661b5b6
Paul Preda <paul.preda@transferwise.com>
2023-06-16 12:10:36 +0100

    removed unncesessary code

--- quiz-frontend/public/react/components/App.js
+++ quiz-frontend/public/react/components/App.js
@@ -2,12 +2,10 @@ import React, { useState, useEffect } from "react";
 import LandingPage from "./LandingPage";
 import QuizList from "./QuizList";
 import PlayingQuiz from "./PlayingQuiz";
-import Button from "@mui/material/Button";
 import HomeIcon from '@mui/icons-material/Home';
 
 import "../../style.css";
 import apiURL from "../api"
-import { Home } from "@mui/icons-material";
 
 export const App = () => {
   const [state, setState] = useState("landing");
@@ -33,7 +31,6 @@ export const App = () => {
   return (
     <main>
       <div className="top-bar">
-      {/* <button onClick={ () => {setState("landing")}} className="top-button">Home</button> */}
       <HomeIcon color="primary" onClick={ () => {setState("landing")}} className="top-button">Home</HomeIcon>
       </div>
 
