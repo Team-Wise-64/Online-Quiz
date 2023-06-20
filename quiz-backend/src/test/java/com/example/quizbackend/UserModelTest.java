e4aa3c472062470e160427241e486145ba9b8c66
Salma Jama <salma.jama@transferwise.com>
2023-06-20 11:37:13 +0100

    Added new test dependency and test file for user

---
+++ quiz-backend/src/test/java/com/example/quizbackend/UserModelTest.java
@@ -0,0 +1,14 @@
+package com.example.quizbackend;
+
+import com.example.quizbackend.model.User;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserModelTest {
+    @Test
+    void userShouldHaveUserId(){
+        System.out.println("This test is working!");
+    }
+}
