493b6213e01cf7e3ea33ca44ad5c73ed095d9614
Salma Jama <salma.jama@transferwise.com>
2023-06-21 11:53:20 +0100

    Added test for scoreRepository and userRepository

--- quiz-backend/src/main/java/com/example/quizbackend/repository/UserRepository.java
+++ quiz-backend/src/main/java/com/example/quizbackend/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.example.quizbackend.repository;
 
 import com.example.quizbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
   public User findAllByUserId(int userId);
---
+++ quiz-backend/src/test/java/com/example/quizbackend/repository/UserRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.quizbackend.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+
+    @Test
+    void findAllByUserId() {
+    }
+}
